73b972fa545501ab1b5002f2195adf4da03d4d51
Vikasguna-HHG <hhgsoftechteam10@gmail.com>
2022-12-21 16:47:04 +0530

    hh

--- controller/api.js
+++ controller/api.js
@@ -18,37 +18,37 @@ exports.insert = async function (req, res, next) {
     //data inser in postman -> body-row-json
     //const data = await demo.create(req.body);
 
-    const data = await demo.create(req.body , req.file);
+    const data = await demo.create(req.body, req.file);
 
     console.log(data);
     res.status(201).json({ status: "success", data: data });
-  } catch (error) {}
+  } catch (error) { }
 };
 
 exports.get = async function (req, res, next) {
   try {
     const data = await demo.find();
     res.status(201).json({ status: "success", data: data });
-  } catch (error) {}
+  } catch (error) { }
 };
 
 exports.get_id = async function (req, res, next) {
   try {
     const data = await demo.findById(req.params.id);
     res.status(200).json({ status: "success", data: data });
-  } catch (error) {}
+  } catch (error) { }
 };
 
 exports.delete = async function (req, res, next) {
   try {
     const data = await demo.findByIdAndDelete(req.params.id);
     res.status(404).json({ status: "delete data" });
-  } catch (error) {}
+  } catch (error) { }
 };
 
 exports.update = async function (req, res, next) {
   try {
     const data = await demo.findByIdAndUpdate(req.params.id, req.body);
     res.status(201).json({ status: "sucess", data: data });
-  } catch (error) {}
+  } catch (error) { }
 };
